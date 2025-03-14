221b3e9d359c04ded11713d33018ed36a60b291e
mihirniyogi <E1122052@u.nus.edu>
2025-03-15 00:10:28 +0800

    Modify view command to allow 'view client' and 'view pastry'

--- src/main/java/powerbake/address/logic/parser/CliSyntax.java
+++ src/main/java/powerbake/address/logic/parser/CliSyntax.java
@@ -7,7 +7,7 @@ public class CliSyntax {
 
     /* Prefix definitions */
     public static final Prefix PREFIX_CLIENT = new Prefix("client ");
-    public static final Prefix PREFIX_PASTRY = new Prefix("pastry");
+    public static final Prefix PREFIX_PASTRY = new Prefix("pastry ");
     public static final Prefix PREFIX_PHONE = new Prefix("-p ");
     public static final Prefix PREFIX_EMAIL = new Prefix("-e ");
     public static final Prefix PREFIX_ADDRESS = new Prefix("-a ");

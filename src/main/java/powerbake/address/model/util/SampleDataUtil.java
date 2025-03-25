b3e401d0627a1e07a22e35bf8026c1ed1ad4f284
mihirniyogi <E1122052@u.nus.edu>
2025-03-25 23:24:05 +0800

    Fix checkstyle issues

--- src/main/java/powerbake/address/model/util/SampleDataUtil.java
+++ src/main/java/powerbake/address/model/util/SampleDataUtil.java
@@ -22,53 +22,56 @@ import powerbake.address.model.tag.Tag;
  * Contains utility methods for populating {@code AddressBook} with sample data.
  */
 public class SampleDataUtil {
-    
+
     public static final Person PERSON_1 = new Person(
-        new Name("Alex Yeoh"), 
-        new Phone("87438807"), 
+        new Name("Alex Yeoh"),
+        new Phone("87438807"),
         new Email("alexyeoh@example.com"),
         new Address("Blk 30 Geylang Street 29, #06-40"),
         getTagSet("friends"));
 
     public static final Person PERSON_2 = new Person(
-            new Name("Bernice Yu"), 
-            new Phone("99272758"), 
+            new Name("Bernice Yu"),
+            new Phone("99272758"),
             new Email("berniceyu@example.com"),
             new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
             getTagSet("colleagues", "friends"));
 
     public static final Person PERSON_3 = new Person(
-            new Name("Charlotte Oliveiro"), 
-            new Phone("93210283"), 
+            new Name("Charlotte Oliveiro"),
+            new Phone("93210283"),
             new Email("charlotte@example.com"),
             new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
             getTagSet("neighbours"));
 
     public static final Person PERSON_4 = new Person(
-            new Name("David Li"), 
-            new Phone("91031282"), 
+            new Name("David Li"),
+            new Phone("91031282"),
             new Email("lidavid@example.com"),
             new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
             getTagSet("family"));
 
     public static final Person PERSON_5 = new Person(
-            new Name("Irfan Ibrahim"), 
-            new Phone("92492021"), 
+            new Name("Irfan Ibrahim"),
+            new Phone("92492021"),
             new Email("irfan@example.com"),
             new Address("Blk 47 Tampines Street 20, #17-35"),
             getTagSet("classmates"));
 
     public static final Person PERSON_6 = new Person(
-            new Name("Roy Balakrishnan"), 
-            new Phone("92624417"), 
+            new Name("Roy Balakrishnan"),
+            new Phone("92624417"),
             new Email("royb@example.com"),
             new Address("Blk 45 Aljunied Street 85, #11-31"),
             getTagSet("colleagues"));
 
-    
-    public static final Pastry PASTRY_1 = new Pastry(new powerbake.address.model.pastry.Name("Apple Pie"), new Price("3.50"));
-    public static final Pastry PASTRY_2 = new Pastry(new powerbake.address.model.pastry.Name("Brownie"), new Price("2.50"));
-    public static final Pastry PASTRY_3 = new Pastry(new powerbake.address.model.pastry.Name("Cheesecake"), new Price("4.00"));
+
+    public static final Pastry PASTRY_1 = new Pastry(
+            new powerbake.address.model.pastry.Name("Apple Pie"), new Price("3.50"));
+    public static final Pastry PASTRY_2 = new Pastry(
+            new powerbake.address.model.pastry.Name("Brownie"), new Price("2.50"));
+    public static final Pastry PASTRY_3 = new Pastry(
+            new powerbake.address.model.pastry.Name("Cheesecake"), new Price("4.00"));
 
     public static Person[] getSamplePersons() {
         return new Person[] {

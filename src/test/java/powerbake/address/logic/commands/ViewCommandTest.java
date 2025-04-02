e768f94a0efb69901fb2ec461382b09567f9a062
cyhni <nigel99_chan@hotmail.com>
2025-04-02 23:13:29 +0800

    Fix test cases

--- src/test/java/powerbake/address/logic/commands/ViewCommandTest.java
+++ src/test/java/powerbake/address/logic/commands/ViewCommandTest.java
@@ -8,6 +8,7 @@ import static powerbake.address.logic.commands.CommandTestUtil.assertCommandFail
 import static powerbake.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static powerbake.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static powerbake.address.testutil.TypicalAddressBook.getTypicalAddressBook;
+import static powerbake.address.testutil.TypicalIndexes.INDEX_FIRST_ORDER;
 import static powerbake.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -28,6 +29,8 @@ public class ViewCommandTest {
     private static final String MESSAGE_SUCCESS_CLIENT = String.format(ViewCommand.MESSAGE_SUCCESS, CLIENT);
     private static final String MESSAGE_SUCCESS_PASTRY = String.format(ViewCommand.MESSAGE_SUCCESS, PASTRY);
     private static final String MESSAGE_SUCCESS_ORDER = String.format(ViewCommand.MESSAGE_SUCCESS, ORDER);
+    private static final String MESSAGE_SUCCESS_INDEX =
+            String.format(ViewCommand.MESSAGE_SUCCESS_INDEX, ORDER, INDEX_FIRST_ORDER.getOneBased());
 
     private Model model;
     private Model expectedModel;
@@ -53,7 +56,7 @@ public class ViewCommandTest {
     @Test
     public void execute_orderListNotFiltered_showsSameList() {
         assertCommandSuccess(new ViewCommand(ORDER), model, MESSAGE_SUCCESS_ORDER, expectedModel);
-        assertCommandSuccess(new ViewCommand(INDEX_FIRST_PERSON), model, MESSAGE_SUCCESS_ORDER, expectedModel);
+        assertCommandSuccess(new ViewCommand(INDEX_FIRST_PERSON), model, MESSAGE_SUCCESS_INDEX, expectedModel);
     }
 
     @Test

bb2d2a5cb0676c5cd2bedfe41d61cbed751e9d9e
cyhni <nigel99_chan@hotmail.com>
2025-04-02 23:22:16 +0800

    Update person to client in command messages

--- src/main/java/powerbake/address/logic/commands/AddClientCommand.java
+++ src/main/java/powerbake/address/logic/commands/AddClientCommand.java
@@ -21,7 +21,7 @@ public class AddClientCommand extends AddCommand {
     public static final String COMMAND_WORD = "add";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + " " + PREFIX_CLIENT
-            + ": Adds a person to the address book. "
+            + ": Adds a client to the address book. "
             + "Parameters: "
             + PREFIX_CLIENT + "NAME "
             + PREFIX_PHONE + "PHONE "
@@ -36,8 +36,8 @@ public class AddClientCommand extends AddCommand {
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 
-    public static final String MESSAGE_SUCCESS = "New person added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
+    public static final String MESSAGE_SUCCESS = "New client added: %1$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This client already exists in the address book";
 
     private final Person toAdd;
 

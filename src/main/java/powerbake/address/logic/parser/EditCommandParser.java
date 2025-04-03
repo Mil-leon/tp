ea6b7a8d1268aa88d41638d18101a1bf76363d2a
Emmanuel <Emmanuel.csh@hotmail.com>
2025-04-03 13:38:00 +0800

    Fix duplicate name for edit name command

--- src/main/java/powerbake/address/logic/parser/EditCommandParser.java
+++ src/main/java/powerbake/address/logic/parser/EditCommandParser.java
@@ -56,7 +56,7 @@ public class EditCommandParser implements Parser<EditCommand> {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE), pe);
         }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS,
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_NAME,
                 PREFIX_PRICE, PREFIX_TAG, PREFIX_STATUS);
 
         if (entityType.equals("client")) {

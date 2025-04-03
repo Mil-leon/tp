46b1041c9849064e35b045df8b7f26c9f270ada0
Mil-leon <noellim30@gmail.com>
2025-04-03 14:50:48 +0800

    Fix checkstyle issue

--- src/main/java/powerbake/address/model/person/Address.java
+++ src/main/java/powerbake/address/model/person/Address.java
@@ -9,8 +9,8 @@ import static powerbake.address.commons.util.AppUtil.checkArgument;
  */
 public class Address {
 
-    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank. " +
-            "It should be between 5 - 100 characters.";
+    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank. "
+            + "It should be between 5 - 100 characters.";
 
     /*
      * The first character of the address must not be a whitespace,
